629f48005102d9ae23d5a76486d2593e5cbb4425
Emmanuel Wiesenfeld <96352851+wiesenfeK@users.noreply.github.com>
2025-04-01 20:11:45 -0400

    Update kawa-python-runner.sh

--- lib/kawa-python-runner.sh
+++ lib/kawa-python-runner.sh
@@ -1,10 +1,6 @@
 #/bin/bash
-
 CONFIG_DIR=/etc/kawa
 LOG_DIR=/var/log/kawa
-TOKEN_FILE=$CONFIG_DIR/deploy.token
-TOKEN=`cat $TOKEN_FILE`
-PACKAGE_URL="https://__token__:$TOKEN@gitlab.com/api/v4/projects/56094807/packages/pypi/simple"
 
 . $CONFIG_DIR/kawa.env
 
@@ -12,6 +8,6 @@ export KAWA_AUTOMATION_SERVER_AES_KEY=$KAWA_GLOBAL_RUNNER_AES_KEY
 export PATH=$(python3 -m site --user-base)/bin:$PATH
 
 pipx install pex
-pipx install kawapythonserver --index-url $PACKAGE_URL
+pipx install kawapythonserver
 
 kawapythonserver > $LOG_DIR/kawapythonserver.log 2>&1
